5635b81f5486584f54bdedd66ed371ad5e371694
Huey <spk.kou2kun@gmail.com>
2023-08-07 22:03:59 +0700

    sync from laptop

---
+++ Redux/udacity-goals-todos/src/components/App.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { handleInitialData } from "../actions/shared";
+import ConnectedTodos from "./Todos";
+import ConnectedGoals from "./Goals";
+import { connect } from "react-redux";
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
+  if (props.loading === true) {
+    return <h3>Loading</h3>;
+  }
+
+  return (
+    <div>
+      <ConnectedTodos />
+      <ConnectedGoals />
+    </div>
+  );
+};
+
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);
